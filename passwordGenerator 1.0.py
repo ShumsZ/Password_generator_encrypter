1d7b4034d6238c346a3d93051db0e8f238bc3184
ShumsZ <67175740+ShumsZ@users.noreply.github.com>
2022-01-17 19:46:15 +0000

    Update passwordGenerator 1.0.py

--- passwordGenerator 1.0.py
+++ passwordGenerator 1.0.py
@@ -4,7 +4,7 @@ import random
 variables = ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "1234567890","£$%&!*"]
 
 result = '' # need to define result variable to store the value of result in for each iteration of the loop
-for i in range(3):
+for i in range(4):
 
     for element in variables:
         result += str(random.choice(element)) 
